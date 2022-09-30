252786d4bbf068a049a39c88cbe34a9140829a23
Uptaker <markus.tammeoja@gmail.com>
2022-10-01 00:02:22 +0300

    add database imports

---
+++ server/src/config.ts
@@ -0,0 +1,7 @@
+export const env = {
+  dbUser: process.env.DB_USER,
+  dbHost: process.env.DB_HOST,
+  dbDatabase: process.env.DB_DATABASE,
+  dbPassword: process.env.DB_PASSWORD,
+  dbPort: process.env.DB_PORT,
+}
