7f212c6d9c9f4587833b9896010ec67cdda0b853
Uptaker <markus.tammeoja@gmail.com>
2022-10-05 19:58:24 +0300

    add a delay to spinner

--- src/common/Spinner.svelte
+++ src/common/Spinner.svelte
@@ -1,4 +1,8 @@
-<div class="w-100 text-center">
+<script lang="ts">
+  import {slide} from 'svelte/transition'
+</script>
+
+<div class="w-100 text-center" transition:slide={{delay:550}}>
   <div class="spinner-border text-primary" role="status">
     <span class="sr-only">Loading...</span>
   </div>
