f7f9e8960c47737af9922423fb8f85a2520707a9
Uptaker <markus.tammeoja@gmail.com>
2022-10-07 00:39:23 +0300

    add a new open-source icon

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -1,6 +1,6 @@
 <div class="bg-white outline navbar-light navbar-expand-lg px-3 px-lg-5 mb-5 rounded">
   <nav id="top" class="navbar py-3 d-flex gap-5 gap-md-3 flex-column justify-content-start flex-md-row p-0">
-      <a class="navbar-brand text-dark fw-bolder p-0 me-lg-4"><i class="fa-solid fa-school"></i> Testkeskond</a>
+      <a class="navbar-brand text-dark fw-bolder p-0 me-lg-4"><img width="40" src="/media/logo.svg" alt="logo"> <span>Testkeskond</span></a>
       <slot/>
   </nav>
   <slot name="authenticated"/>
