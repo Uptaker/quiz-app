2aa4cd6eb6216f4ad245fdb6fb18256ddea1a8af
Uptaker <markus.tammeoja@gmail.com>
2022-10-08 01:11:08 +0300

    fix fetch

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -2,12 +2,15 @@ import {writable} from 'svelte/store'
 
 export const isAdmin = writable<boolean>(false)
 
-
 export function initSession() {
   isAdmin.set(true)
 }
 
 export async function logout() {
-  await fetch('/api/user/logout', {method: 'POST'})
-  setTimeout(() => location.href = '/', 0)
+  try {
+    await fetch('/api/user/logout', {method: 'POST'})
+    setTimeout(() => location.href = '/', 0)
+  } catch (e) {
+    setTimeout(location.href = '/', 0)
+  }
 }
