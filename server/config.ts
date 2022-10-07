05da6bb214500be7ee2731322aef4cb2a01120dc
Uptaker <markus.tammeoja@gmail.com>
2022-10-08 01:05:47 +0300

    use env variables & move it to root so that docker-compose can read it

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,6 @@
 export const config = {
-  ADMIN_PASSWORD: 'test',
-  ADMIN_USER: 'test'
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  ADMIN_USER: process.env.ADMIN_USERNAME,
+  COOKIE_SECRET: process.env.COOKIE_SECRET ?? 'YourCookieValueHerePleaseChangeItToSomethingElse',
+  SESSION_SECRET: process.env.SESSION_SECRET ?? 'YourSessionSecretHerePleaseChangeItToSomethingElse'
 }
