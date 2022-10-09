edce9c1064a834df4f3ef8672b954ae578d64cf9
Uptaker <markus.tammeoja@gmail.com>
2022-10-09 21:03:25 +0300

    mobile optimizations

--- src/routes/tests/QuizRow.svelte
+++ src/routes/tests/QuizRow.svelte
@@ -11,25 +11,27 @@
 <Card flex="d-flex flex-column flex-sm-row justify-content-between align-items-center gap-3 gap-md-5 w-100" padding="px-4 py-3">
   <div>
     <div class="fw-bolder mb-3">{quiz.name}</div>
-    <div class="text-small d-inline fw-bolder text-primary me-3">#{formatUuid(quiz.uuid)}</div>
+    <div class="d-flex flex-column flex-md-row gap-3 w-100 align-items-center">
+      <div class="text-small d-inline fw-bolder text-primary me-3">#{formatUuid(quiz.uuid)}</div>
       {#if $isAdmin}
         <div class="text-small d-inline fw-bolder text-primary me-3">
           {#if quiz.updatedAt}
-            Muudetud&nbsp;&nbsp;<span class="fw-normal">{dateTime(quiz.updatedAt)}</span>
+            Muudatud&nbsp;&nbsp;<span class="fw-normal">{dateTime(quiz.updatedAt)}</span>
           {:else}
             Koostatud&nbsp;&nbsp;<span class="fw-normal">{dateTime(quiz.createdAt)}</span>
           {/if}
         </div>
         <LinkTo to="/quiz/{quiz.uuid}/edit" class="btn btn-sm btn-light fw-bolder text-small text-primary">
-          <i class="fa-solid fa-pen-to-square"></i>&nbsp;&nbsp;Muuda
+          <i class="fa-solid fa-pen-to-square"></i>&nbsp;&nbsp;<span>Muuda</span>
         </LinkTo>
-    {/if}
+      {/if}
+    </div>
 
   </div>
-  <div>
+  <div class="d-flex flex-row flex-md-column gap-4 justify-content-between align-items-center">
     <LinkTo to="/quiz/{quiz.uuid}" class="btn btn-lg btn-primary"><i class="fa-solid fa-play"></i>&nbsp;&nbsp; Soorita</LinkTo>
     {#if quiz.size}
-      <div class="text-small text-primary mt-2 text-center"><span class="fw-bolder">{quiz.size}</span>&nbsp;küsimust</div>
+      <div class="text-small text-primary text-center"><span class="fw-bolder">{quiz.size}</span>&nbsp;küsimust</div>
     {/if}
   </div>
 </Card>
