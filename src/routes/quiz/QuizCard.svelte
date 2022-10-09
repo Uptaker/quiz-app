23e83b7df2663050809b0465e381ec18216f838c
Uptaker <markus.tammeoja@gmail.com>
2022-10-09 21:03:10 +0300

    make the quiz count blue

--- src/routes/quiz/QuizCard.svelte
+++ src/routes/quiz/QuizCard.svelte
@@ -6,13 +6,12 @@
   export let question: QuizQuestion
   export let questionAmount: number
   export let index: number
-  let imageError = false
 </script>
 
 <Card fullWidth flex="d-flex justify-content-between align-items-center gap-4 gap-lg-5" padding="px-3 px-md-4 px-lg-5 py-4">
   <div class="d-flex justify-content-between w-100 gap-2">
     <span class="lead">{question.question}</span>
-    <span class="fw-bolder text-small">{index}&nbsp;/&nbsp;{questionAmount}</span>
+    <span class="fw-bolder text-small text-primary">{index}&nbsp;/&nbsp;{questionAmount}</span>
   </div>
 
   <QuizImage image={question.pictureName} />
